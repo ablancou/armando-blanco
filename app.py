042cc1d51261c01194901ceedaa2a49083f3b7f8
Armando <ablancour@gmail.com>
2025-11-09 04:13:54 -0600

    Update app.py
    Update

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ from PIL import Image
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
+# CSS for Summary (darker blue transparent background, light blue text, 3D shadow)
+st.markdown("""
+<style>
+  div.stAlert > div {
+    background-color: rgba(0, 0, 139, 0.8) !important;
+    color: #add8e6 !important;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2) !important;
+  }
+</style>
+""", unsafe_allow_html=True)
+
 #####################
 # Header
 st.write('''
 # Armando Blanco
-##### *Resume* - Software Engineer II Candidate
+### Resume
+#### Software Engineer II Candidate
 ''')
 image = Image.open('AB.png')
 st.image(image, width=125)
@@ -198,7 +210,7 @@ These tools have enabled me to develop impactful projects, such as interactive a
 # Dict of icon URLs (validated with tool)
 icon_urls = {
     'C++': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/182px-ISO_C%2B%2B_Logo.svg.png',
-    'Python': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png',  # Better Python logo
+    'Python': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png', # Better Python logo
     'JavaScript/TypeScript': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/240px-JavaScript-logo.png',
     'C': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/The_C_Programming_Language_logo.svg/120px-The_C_Programming_Language_logo.svg.png',
     'C#': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/512px-C_Sharp_Logo_2023.svg.png',
@@ -213,10 +225,10 @@ icon_urls = {
     'Streamlit': 'https://streamlit.io/images/brand/streamlit-mark-color.png',
     'SQL': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sql_data_base_with_logo.png/512px-Sql_data_base_with_logo.png',
     'NoSQL': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
-    'Git/GitHub': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',  # Better Git size
+    'Git/GitHub': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg', # Better Git size
     'Agile Methodologies': 'https://cdn.iconscout.com/icon/free/png-256/free-agile-3629027-3030163.png',
     'CI/CD Pipelines': 'https://cdn.iconscout.com/icon/free/png-256/free-ci-cd-3629028-3030164.png',
-    'Docker': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg'  # Better Docker size
+    'Docker': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg' # Better Docker size
 }
 
 # Subcategories with txt4_with_icon
