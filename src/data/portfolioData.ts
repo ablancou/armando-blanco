5e284695b96833f8bec06fdb3a9b0dfbe23e0548
“ablancou“ <16216295+ablancou@users.noreply.github.com>
2026-03-25 04:47:33 -0600

    content: update Goodreads profile url

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -81,7 +81,7 @@ export const socials = [
   { name: "GitHub", url: "https://github.com/ablancou", icon: "Github" },
   { name: "X", url: "https://x.com/ablancou", icon: "Twitter" },
   { name: "Duolingo", url: "https://www.duolingo.com/profile/ablancou", icon: "Duolingo" },
-  { name: "Goodreads", url: "https://www.goodreads.com/user/show/TU_ID", icon: "Goodreads" }
+  { name: "Goodreads", url: "https://www.goodreads.com/ablancou", icon: "Goodreads" }
 ];
 
 
